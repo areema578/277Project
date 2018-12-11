4730da6c5a09ee805d8043d62737b41e4654a056
Howard M Chen <31971182+hchen88@users.noreply.github.com>
2018-12-10 20:52:33 -0800

    Add files via upload

--- SuiteRoom.java
+++ SuiteRoom.java
@@ -1,6 +1,13 @@
 package patternsProject;
 
+import java.util.ArrayList;
+
 public class SuiteRoom extends MotelRoom{
 	
+	public SuiteRoom(RoachColony roachColony, String type, ArrayList<Amenity> ammenities, int roomNumber) {
+		super(roachColony, type, ammenities, roomNumber);
+		// TODO Auto-generated constructor stub
+	}
+
 	private static double cost = 100;
 }
