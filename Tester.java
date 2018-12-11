fe0eb8d384c4c9f61b2b0b20d745436754eeab61
Howard M Chen <31971182+hchen88@users.noreply.github.com>
2018-12-10 21:34:14 -0800

    Add files via upload

--- Tester.java
+++ Tester.java
@@ -9,11 +9,25 @@ public class Tester {
 		rm.createRooms();
 		System.out.println(rm);
 		RoachColony rc1 = new RoachColony("First Colony", 100, 200);
-		ArrayList amenities = new ArrayList();
-        amenities.add("foodbar");
-        amenities.add("spa");
-        amenities.add("refillbar");
-        amenities.add("shower");
+		ArrayList<String> amenities = new ArrayList<String>();
+        amenities.add("Foodbar");
+        amenities.add("Spa");
+        amenities.add("RefillFood");
+        amenities.add("Shower");
+        
+		RoachColony colony1 = new RoachColony("1st Colony", 5, 0.5);
+		RoachColony colony2 = new RoachColony("2nd Colony", 10, 1.5);
+		RoachColony colony3 = new RoachColony("3rd Colony", 25, 0.35);
+		RoachColony colony4 = new RoachColony("4th Colony", 40, 0.75);
+		RoachColony colony5 = new RoachColony("5th Colony", 36, 2.0);
+		RoachColony colony6 = new RoachColony("6th Colony", 89, .30);
+		RoachColony colony7 = new RoachColony("7th Colony", 55, 0.75);
+		
+        RoachMotel rMotel = RoachMotel.getInstance();
+        rMotel.createRooms(); // creates rooms
+        amenities.add("FoodBar");
+        rMotel.checkIn(colony1, "Regular", amenities);
+
 	}
 	
 }
